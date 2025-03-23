25073f701798b0a15c8a0ba84d2edd2e8022dbc7
Lloyd <lloyd@lloydtheandroid.com>
2025-03-23 18:36:12 +0000

    updated image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function LandingPage() {
             <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4 dark:bg-white/5 dark:ring-white/10">
                 <img
-                  src="/images/dashboard-preview.png"
+                  src="/images/dashboard-preview.jpeg"
                   alt="App screenshot"
                   width={2432}
                   height={1442}
